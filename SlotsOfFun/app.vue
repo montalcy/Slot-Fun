9187b0accfea51eca1c36cf07e987154eac3bb90
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-08-07 19:51:43 -0700

    Adding fun title

---
+++ SlotsOfFun/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="font" style="text-align: center;">
+      Welcome to Slots of Fun
+    </h1>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nabla&display=swap');
+.font{
+  font-family: 'Nabla';
+}
+</style>
