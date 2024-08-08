90b5a37066e3274954708e4b39e91a9189f6d7f2
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-08-07 21:13:14 -0700

    adding play button

--- SlotsOfFun/app.vue
+++ SlotsOfFun/app.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>
-    <h1 class="font" style="text-align: center;">
-      Welcome to Slots of Fun
-    </h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage>
+      <div>
+        <h1 class="font" style="text-align: center;">Welcome to Slots of Fun</h1>
+        <div class="text-center pa-16">
+          
+          <v-btn
+          rounded
+          variant="elevated"
+            size="x-large"
+            @click="dialog = true"
+            style="
+              font-weight: bold;
+              color: greenyellow;
+              background-color: orangered;
+            "
+          >
+            <v-icon>mdi-play-circle-outline</v-icon>Let's Play
+          </v-btn>
+        </div>
+      </div>
+    </NuxtPage>
+  </NuxtLayout>
 </template>
 
+<script></script>
+
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Nabla&display=swap');
-.font{
-  font-family: 'Nabla';
+@import url("https://fonts.googleapis.com/css2?family=Nabla&display=swap");
+.font {
+  font-family: "Nabla";
+}
+
+.btn-style {
+  font-weight: bold;
+  color: yellowgreen;
+  background-color: orangered;
 }
 </style>
--- SlotsOfFun/pages/index.vue
+++ SlotsOfFun/pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-    
-</template>
+    <v-row justify="center">
+      <v-col v-for="k in 2" :key="k" cols="4">
+        <v-sheet class="pa-2 ma-2"> .justify-center </v-sheet>
+      </v-col>
+    </v-row>
+  </template>
+  
