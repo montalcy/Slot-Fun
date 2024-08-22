2176e4870cbd5fe08981956b250944c53055b341
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-08-21 19:16:46 -0700

    Pop up moved and functional
    moved the modal component to the game page so that I could overlay it and make the background transparent. The button in the modal will put you back in the game, as well.

--- SlotsOfFun/pages/game.vue
+++ SlotsOfFun/pages/game.vue
@@ -15,12 +15,25 @@
           HOME
         </v-btn>
       </div>
+
+<!-- THIS IS FOR THE POP UP -->
+   <div v-if="showModal" class="modal-overlay">
+    <div class="modal" style="background-color: white;">
+      <div :style="fontStyle" style="color:black; font-size: xx-large;">{{ resultMessage }}</div>
+      <v-btn :style="fontStyle" style="color:black; margin-top: 5%;" @click="closeModal">
+        Play Again!
+      </v-btn>
+    </div>
+  </div>
+
+
+
     </main>
   </template>
 
   
   <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { ref, watch } from 'vue';
   import { useRouter } from 'vue-router';
   import { getRandomFruit, checkRandomFruit, fruits } from '~/scripts/spinRandom';
   import { resultValue } from './sharedState';
@@ -32,28 +45,42 @@
   const randomFruit = ref('');
   const randomFruit2 = ref('');
   const randomFruit3 = ref('');
+  const showModal = ref(false);
   
   function spin() {
       randomFruit.value = getRandomFruit();
       randomFruit2.value = getRandomFruit();
       randomFruit3.value = getRandomFruit();
-      if (!checkRandomFruit(randomFruit.value, randomFruit2.value, randomFruit3.value)) {
-        console.log('try again!');
-        resultValue.value = false;
-      }
-      if (checkRandomFruit(randomFruit.value, randomFruit2.value, randomFruit3.value)) {
-        console.log('you won!');
-        resultValue.value = true;
-      }   
+      resultValue.value = checkRandomFruit(randomFruit.value, randomFruit2.value, randomFruit3.value);
+     
   }
 
   function spinAndRoute(){
     spin();
        setTimeout(() => {
-      router.push('/results');
-  }, 700); 
+        showModal.value = true;
+  }, 900); 
    
   }
+
+// THIS IS FOR POPUP
+
+const resultMessage = ref('');
+function closeModal() {
+showModal.value = !showModal.value;
+}
+
+
+function showResultMessage(res: boolean) {
+  console.log('RESULTS : '+res);
+    resultMessage.value = res ? "YOU'VE WON! :)" : "You've lost :(";
+}
+
+watch(resultValue, (newResult) => {
+  console.log('New Value:', newResult);  
+  showResultMessage(newResult);
+}, { immediate: true });
+
   </script>
   
   <style>
@@ -85,5 +112,26 @@
     font-size: 40px;
     background-color: black;
   }
+ /* STYLE FOR POPUP */
+  .modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5); /* Adjust opacity here (0 to 1) */}
+
+.modal {
+  text-align: center;
+  height: 200px;
+  width: 400px;
+  margin-top: 20%;
+  padding: 60px 0;
+  border-radius: 20px;
+  background-color: rgba(255, 255, 255, 0.8); /* Adjust opacity here (0 to 1) */
+
+}
   </style>
   
--- SlotsOfFun/scripts/spinCheck.ts
+++
@@ -1,6 +0,0 @@
-
-
-// export function getFruitSpin(fruits: string 1, string 2, string 3){
-//     if (1==(2) && 1 ) {
-// \
-//   }
